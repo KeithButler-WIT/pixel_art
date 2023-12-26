bd997b223ff83e095410f524568dd8d68f88ec04
Keith Butler <keithbutler2001@gmail.com>
2023-12-26 11:32:32 +0000

    Initial commit

---
+++ w_Isometric.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="B/w_Isometric" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="B_W_Isometric.png" width="160" height="128"/>
+</tileset>
